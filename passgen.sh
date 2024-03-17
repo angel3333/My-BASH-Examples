435ab18f7c877cbfb11ddd70fc35ce319e65fec4
meleksabit <angel3.85bg@gmail.com>
2024-03-18 01:13:20 +0200

    edit passgen.sh

--- passgen.sh
+++ passgen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Please enter the password length:"
-read length
+read -r length
 password=$(openssl rand -base64 16 | tr -dc 'a-zA-Z0-9' | head -c "$length")
 echo "Generated password: $password"
 echo -e "Password length: " echo -n "$password" | wc -c
